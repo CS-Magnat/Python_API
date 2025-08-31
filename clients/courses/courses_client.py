0bf6a8f223977b431afc7515023b2b95f71be093
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2025-09-01 00:12:17 +0500

    + api_client_create_course.py + api_client_create_exercise.py + api_client_get_user.py

--- clients/courses/courses_client.py
+++ clients/courses/courses_client.py
@@ -3,7 +3,27 @@ from typing import TypedDict
 from httpx import Response
 
 from clients.api_client import APIClient
+from clients.files.files_client import File
 from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
+from clients.users.private_users_client import User
+
+
+
+# Добавили описание структуры курса
+class Course(TypedDict):
+    """
+    Описание структуры курса.
+    """
+    id: str
+    title: str
+    maxScore: int
+    minScore: int
+    description: str
+    previewFile: File  # Вложенная структура файла
+    estimatedTime: str
+    createdByUser: User  # Вложенная структура пользователя
+
+
 
 class GetCoursesQueryDict(TypedDict):
     """
@@ -24,6 +44,14 @@ class CreateCourseRequestDict(TypedDict):
     previewFileId: str
     createdByUserId: str
 
+# Добавили описание структуры запроса на создание курса
+class CreateCourseResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания курса.
+    """
+    course: Course
+
+
 
 class UpdateCourseRequestDict(TypedDict):
     """
@@ -34,6 +62,9 @@ class UpdateCourseRequestDict(TypedDict):
     minScore: int | None
     description: str | None
     estimatedTime: str | None
+
+
+
 
 
 class CoursesClient(APIClient):
@@ -87,6 +118,13 @@ class CoursesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.delete(f"/api/v1/courses/{course_id}")
+
+
+    # Добавили новый метод
+    def create_course(self, request: CreateCourseRequestDict) -> CreateCourseResponseDict:
+        response = self.create_course_api(request)
+        return response.json()
+
 
 # Добавляем builder для CoursesClient
 def get_courses_client(user: AuthenticationUserDict) -> CoursesClient:
