68fe4f2dbd596022215891217869fe570ce34ef5
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2025-09-04 22:44:27 +0500

    + files_schema.py

--- api_client_create_course.py
+++ api_client_create_course.py
@@ -1,13 +1,17 @@
 from clients.courses.courses_client import get_courses_client, CreateCourseRequestDict
-from clients.files.files_client import get_files_client, CreateFileRequestDict
+from clients.files.files_client import get_files_client
 from clients.private_http_builder import AuthenticationUserSchema
-from clients.users.public_users_client import get_public_users_client, CreateUserRequestDict
+from clients.users.public_users_client import get_public_users_client
+# Вместо CreateUserRequestDict импортируем CreateUserRequestSchema
+from clients.users.users_schema import CreateUserRequestSchema
+# Вместо CreateFileRequestDict импортируем CreateFileRequestSchema
+from clients.files.files_schema import CreateFileRequestSchema
 from tools.fakers import get_random_email
 
 public_users_client = get_public_users_client()
 
 # Создаем пользователя
-create_user_request = CreateUserRequestDict(
+create_user_request = CreateUserRequestSchema(
     email=get_random_email(),
     password="string",
     lastName="string",
@@ -18,14 +22,15 @@ create_user_response = public_users_client.create_user(create_user_request)
 
 # Инициализируем клиенты
 authentication_user = AuthenticationUserSchema(
-    email=create_user_request['email'],
-    password=create_user_request['password']
+    email=create_user_request.email,
+    password=create_user_request.password
 )
 files_client = get_files_client(authentication_user)
 courses_client = get_courses_client(authentication_user)
 
 # Загружаем файл
-create_file_request = CreateFileRequestDict(
+# Вместо CreateFileRequestDict используем CreateFileRequestSchema
+create_file_request = CreateFileRequestSchema(
     filename="hold.jpg",
     directory="courses",
     upload_file="/Users/uladzimirrudnik/PycharmProjects/Python_API/testdata/files/hold.jpg"
@@ -40,8 +45,8 @@ create_course_request = CreateCourseRequestDict(
     minScore=10,
     description="Python API course",
     estimatedTime="2 weeks",
-    previewFileId=create_file_response['file']['id'],
-    createdByUserId=create_user_response['user']['id']
+    previewFileId=create_file_response.file.id,  # Используем атрибуты место ключей
+    createdByUserId=create_user_response.user.id  # Используем атрибуты место ключей
 )
 create_course_response = courses_client.create_course(create_course_request)
 print('Create course data:', create_course_response)
--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -1,14 +1,18 @@
 from clients.courses.courses_client import get_courses_client, CreateCourseRequestDict
 from clients.exercises.exercises_client import CreateExercisesRequestDict, get_exercises_client
-from clients.files.files_client import get_files_client, CreateFileRequestDict
+from clients.files.files_client import get_files_client
+# Вместо CreateFileRequestDict импортируем CreateFileRequestSchema
+from clients.files.files_schema import CreateFileRequestSchema
 from clients.private_http_builder import AuthenticationUserSchema
-from clients.users.public_users_client import get_public_users_client, CreateUserRequestDict
+from clients.users.public_users_client import get_public_users_client
+# Вместо CreateUserRequestDict импортируем CreateUserRequestSchema
+from clients.users.users_schema import CreateUserRequestSchema
 from tools.fakers import get_random_email
 
 public_users_client = get_public_users_client()
 
 # Создаем пользователя
-create_user_request = CreateUserRequestDict(
+create_user_request = CreateUserRequestSchema(
     email=get_random_email(),
     password="string",
     lastName="string",
@@ -19,14 +23,15 @@ create_user_response = public_users_client.create_user(create_user_request)
 
 # Инициализируем клиенты
 authentication_user = AuthenticationUserSchema(
-    email=create_user_request['email'],
-    password=create_user_request['password']
+    email=create_user_request.email,
+    password=create_user_request.password
 )
 files_client = get_files_client(authentication_user)
 courses_client = get_courses_client(authentication_user)
 
 # Загружаем файл
-create_file_request = CreateFileRequestDict(
+# Вместо CreateFileRequestDict используем CreateFileRequestSchema
+create_file_request = CreateFileRequestSchema(
     filename="hold.jpg",
     directory="courses",
     upload_file="/Users/uladzimirrudnik/PycharmProjects/Python_API/testdata/files/hold.jpg"
@@ -41,8 +46,8 @@ create_course_request = CreateCourseRequestDict(
     minScore=10,
     description="Python API course",
     estimatedTime="2 weeks",
-    previewFileId=create_file_response['file']['id'],
-    createdByUserId=create_user_response['user']['id']
+    previewFileId=create_file_response.file.id,  # Используем атрибуты место ключей
+    createdByUserId=create_user_response.user.id  # Используем атрибуты место ключей
 )
 create_course_response = courses_client.create_course(create_course_request)
 print('Create course data:', create_course_response)
@@ -57,7 +62,6 @@ create_exercise_request = CreateExercisesRequestDict(
   orderIndex=0,
   description="Python_exercise",
   estimatedTime="3 weeks"
-
 )
 
 exercise_client = get_exercises_client(authentication_user)
