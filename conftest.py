694238ff4633e7f6172606711e5fbba0fafbed29
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2025-09-24 23:38:38 +0800

    + fixtures for courses.py

--- conftest.py
+++ conftest.py
@@ -2,4 +2,5 @@ pytest_plugins = (
     "fixtures.users",
     "fixtures.authentication"
     "fixtures.files",  # Добавляем фикстуры для работы с файлами
+    "fixtures.courses",  # Добавляем фикстуры для работы с курсами
 )
