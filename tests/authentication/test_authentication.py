659ad4a2147903b723abf2a459baa2cd55e27afd
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2025-09-27 23:30:38 +0800

    + add parametrize for classes

--- tests/authentication/test_authentication.py
+++ tests/authentication/test_authentication.py
@@ -13,19 +13,22 @@ from tools.assertions.schema import validate_json_schema
 
 @pytest.mark.authentication
 @pytest.mark.regression
-def test_login(
-        function_user: UserFixture,  # Используем фикстуру для создания пользователя
-        authentication_client: AuthenticationClient
-):
+class TestAuthentication:
+    def test_login(
+            self,
+            function_user: UserFixture,  # Используем фикстуру для создания пользователя
+            public_users_client: PublicUsersClient,
+            authentication_client: AuthenticationClient
+    ):
 
-    # Запрос на логин (login_request -> request)
-    request = LoginRequestSchema(email=function_user.email, password=function_user.password)
-    # Выполняем логин (login_response -> response)
-    response = authentication_client.login_api(request)
-    # Валидация ответа (login_response_data -> response_data)
-    response_data = LoginResponseSchema.model_validate_json(response.text)
+        # Запрос на логин (login_request -> request)
+        request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+        # Выполняем логин (login_response -> response)
+        response = authentication_client.login_api(request)
+        # Валидация ответа (login_response_data -> response_data)
+        response_data = LoginResponseSchema.model_validate_json(response.text)
 
-    assert_status_code(response.status_code, HTTPStatus.OK)
-    assert_login_response(response_data)
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_login_response(response_data)
 
-    validate_json_schema(response.json(), response_data.model_json_schema())
+        validate_json_schema(response.json(), response_data.model_json_schema())
--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -15,35 +15,34 @@ import pytest  # Импортируем библиотеку pytest
 from tools.fakers import fake
 
 
-@pytest.mark.parametrize('email',["mail.ru", "gmail.com", "example.com"])
 @pytest.mark.users  # Добавили маркировку users
 @pytest.mark.regression  # Добавили маркировку regression
-def test_create_user(email: str, public_users_client: PublicUsersClient):# Используем фикстуру API клиента
+class TestUsers:
+    @pytest.mark.parametrize('email', ["mail.ru", "gmail.com", "example.com"])
+    def test_create_user(self, email: str, public_users_client: PublicUsersClient):# Используем фикстуру API клиента
 
-    # Формируем тело запроса на создание пользователя
-    request = CreateUserRequestSchema(email=fake.email(email))
-    # Отправляем запрос на создание пользователя
-    response = public_users_client.create_user_api(request)
-    # Инициализируем модель ответа на основе полученного JSON в ответе
-    # Также благодаря встроенной валидации в Pydantic дополнительно убеждаемся, что ответ корректный
-    response_data = CreateUserResponseSchema.model_validate_json(response.text)
-
-
-    # Используем функцию для проверки статус-кода
-    assert_status_code(response.status_code, HTTPStatus.OK)
-    # Используем функцию для проверки ответа создания юзера
-    assert_create_user_response(request, response_data)
-    # Проверяем, что тело ответа соответствует ожидаемой JSON-схеме
-    validate_json_schema(response.json(), response_data.model_json_schema())
+        # Формируем тело запроса на создание пользователя
+        request = CreateUserRequestSchema(email=fake.email(email))
+        # Отправляем запрос на создание пользователя
+        response = public_users_client.create_user_api(request)
+        # Инициализируем модель ответа на основе полученного JSON в ответе
+        # Также благодаря встроенной валидации в Pydantic дополнительно убеждаемся, что ответ корректный
+        response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
 
-@pytest.mark.users
-@pytest.mark.regression
-def test_get_user_me(private_users_client: PrivateUsersClient, function_user: UserFixture):
+        # Используем функцию для проверки статус-кода
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        # Используем функцию для проверки ответа создания юзера
+        assert_create_user_response(request, response_data)
+        # Проверяем, что тело ответа соответствует ожидаемой JSON-схеме
+        validate_json_schema(response.json(), response_data.model_json_schema())
 
-    response_user_me_api = private_users_client.get_user_me_api()
-    response_user_me_api_data = GetUserResponseSchema.model_validate_json(response_user_me_api.text)
 
-    assert_status_code(response_user_me_api.status_code, HTTPStatus.OK)
-    assert_get_user_response(GetUserResponseSchema.model_validate_json(response_user_me_api.text), function_user.response)
-    validate_json_schema(response_user_me_api.json(), response_user_me_api_data.model_json_schema())
+    def test_get_user_me(self, private_users_client: PrivateUsersClient, function_user: UserFixture):
+
+        response_user_me_api = private_users_client.get_user_me_api()
+        response_user_me_api_data = GetUserResponseSchema.model_validate_json(response_user_me_api.text)
+
+        assert_status_code(response_user_me_api.status_code, HTTPStatus.OK)
+        assert_get_user_response(GetUserResponseSchema.model_validate_json(response_user_me_api.text), function_user.response)
+        validate_json_schema(response_user_me_api.json(), response_user_me_api_data.model_json_schema())
