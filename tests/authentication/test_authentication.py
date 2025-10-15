9872fa65cc36cdbc3988caf34a9ccb1bac743e9a
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2025-10-15 22:47:24 +0800

    + created tags: epics / features / story

--- tests/authentication/test_authentication.py
+++ tests/authentication/test_authentication.py
@@ -8,6 +8,9 @@ from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema
 from fixtures.users import UserFixture  # Заменяем импорт
 from tools.allure.tags import AllureTag  # Импортируем enum с тегами
+from tools.allure.epics import AllureEpic  # Импортируем enum AllureEpic
+from tools.allure.features import AllureFeature  # Импортируем enum AllureFeature
+from tools.allure.stories import AllureStory  # Импортируем enum AllureStory
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
@@ -15,7 +18,10 @@ from tools.assertions.schema import validate_json_schema
 @pytest.mark.authentication
 @pytest.mark.regression
 @allure.tag(AllureTag.REGRESSION, AllureTag.AUTHENTICATION)  # Добавили теги
+@allure.epic(AllureEpic.LMS)  # Добавили epic
+@allure.feature(AllureFeature.AUTHENTICATION)  # Добавили feature
 class TestAuthentication:
+    @allure.story(AllureStory.LOGIN)  # Добавили story
     @allure.title("Login with correct email and password")  # Добавили заголовок
     def test_login(
             self,
